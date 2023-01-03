546b4edf798bfa0cb29569a2cd9bdcbfd3e8b54a
Kesavi-Kanesalingam <kesavi105@gmail.com>
2023-01-03 12:09:06 +0530

    sub added

---
+++ sub.java
@@ -0,0 +1,10 @@
+class Sub {
+    public static void main(String args[]) {
+        int a = 3;
+        int b = 8;
+        int c = 2;
+        int c=10;
+        System.out.println(a-b);
+    }
+}
+//imp file
