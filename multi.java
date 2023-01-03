1de696a1b4b737954cc14c41e69ef927db9576b9
Kesavi-Kanesalingam <kesavi105@gmail.com>
2023-01-03 12:17:05 +0530

    multi added

---
+++ multi.java
@@ -0,0 +1,9 @@
+class Multi {
+    public static void main(String args[]) {
+        int a = 3;
+        int b = 8;
+        int c = 2;
+        int c=10;
+        System.out.println(a*b);
+    }
+}
